fdff7f51fc9e4a002840169f57b1f5c943504438
luffy-2004 <ananthumohan2004@gmail.com>
2024-07-26 21:02:29 +0530

    photo app

---
+++ src/components/First.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Navbar from './Navbar'
+
+const First = () => {
+    return (
+        <div>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <div className="row g-3">
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <Navbar />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Title</label>
+                                <input type="text" className="form-control" placeholder='Enter title name' />
+                            </div>
+                            <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                <label htmlFor="" className="form-label">Thumbnail</label>
+                                <input type="text" className="form-control" placeholder='Enter thumbnail' />
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <label htmlFor="" className="form-label">Image URL</label>
+                                <input type="text" className="form-control" placeholder='Enter image URL' />
+                            </div>
+                            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                                <button className="btn btn-success">SUBMIT</button>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default First
